212097ef4783366276c378ea7628214f78c9ebed
GrahamPerich <graham.perich@gmail.com>
2017-02-15 17:47:26 -0800

    build out intro CLI sequence

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-console.log('hi');
+// .typed method is async because of timeouts and effects; functions passed as callbacks to guarantee order
 
 $(function(){
-  $('#main').typed({
+  $('#line-one').typed({
     strings: ["Booting up... ^1000", "SUCCESS ^1000"],
     typeSpeed: 5,
     showCursor: false,
-    callback: clearText
+    callback: setTextAndInit.bind(this, 'grahamperich@TheCloud ~')
   });
 });
 
-function clearText(){
-  $('#main').text('');
+// initiate app after intro sequence completes
+function setTextAndInit(str) {
+  console.log('starting main app sequence');
+  $('#line-one').text(str);
+  $('#command-line').append(`<span id="line-two"></span>`);
+
+  $('#line-two').typed({
+    strings: ["^1000 ls ^1500"],
+    typeSpeed: 5,
+    showCursor: true,
+    callback: clearText.bind(this, '#line-two', ls.bind(this, ['Projects', 'About-Graham', 'Contact']))
+  });
+}
+
+function clearText(id, callback, args) {
+  console.log('clearing text');
+  $(`${id}`).text('');
+
+  if (callback) {
+    // pass args to cb (optionally) as needed
+    args && args.length > 0 ? callback.apply(this, args) : callback()
+  }
+}
+
+function ls(items) {
+  $('#command-line').append('<br>');
+  $('#command-line').append(`<span id="line-three"></span>`);
+
+  items.forEach(item => {
+    $('#line-three').append(`<span class="cli-file">${item}</span>`);
+  })
+
 }
